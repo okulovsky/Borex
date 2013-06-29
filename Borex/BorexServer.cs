d049edd73ff2b4c861d3f3d8238dd5ac4d49dfae
Eric <Eric.Cartman@gmail.com>
2013-06-29 14:20:07 +0600

    Добавили библиотеку Borex.csproj и выдачу курсов валют

---
+++ Borex/BorexServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Borex
+{
+    public class BorexServer
+    {
+        public IEnumerable<Rate> GetRates()
+        {
+            yield return new Rate(Currencies.USD, 35, 34);
+            yield return new Rate(Currencies.EUR, 45, 44);
+            yield return new Rate(Currencies.PLN, 11, 10);
+            yield return new Rate(Currencies.CZK, 1.6, 1.5);
+
+        }
+    }
+}
---
+++ Borex/Rate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Borex
+{
+    public class Rate
+    {
+        public readonly Currencies Currency;
+        public readonly double BuyRate;
+        public readonly double SellRate;
+        public Rate(Currencies currency, double buyRate, double sellRate)
+        {
+            Currency = currency;
+            BuyRate = buyRate;
+            SellRate = sellRate;
+        }
+    }
+}
