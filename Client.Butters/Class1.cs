408c1823069c5c3a28a130b46708087b2bfb8294
Butters <Leopold.Scotch@gmail.com>
2013-06-29 17:30:29 +0600

    Добавлен Client.Butters.csproj

---
+++ Client.Butters/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Borex;
+
+public class Program
+{
+    public static void Main()
+    {
+        var server = new BorexServer();
+        var account = new Account();
+        account[Currencies.USD] = 100;
+        server.Exchange(account, Currencies.USD, Currencies.EUR, 50);
+        Console.ReadKey();
+    }
+
+}
