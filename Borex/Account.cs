ca74882fc1b3972fc661afbbd2202c1c342720bc
Eric <Eric.Cartman@gmail.com>
2013-06-29 15:11:17 +0600

    Добавил конверсию валют

---
+++ Borex/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Borex
+{
+    public class Account
+    {
+        double[] deposits;
+        public double this[Currencies Currency]
+        {
+            get { return deposits[(int)Currency]; }
+            set { deposits[(int)Currency] = value; }
+        }
+    }
+}
--- Borex/BorexServer.cs
+++ Borex/BorexServer.cs
@@ -13,8 +13,14 @@ namespace Borex
             yield return new Rate(Currencies.EUR, 45, 44);
             yield return new Rate(Currencies.PLN, 11, 10);
             yield return new Rate(Currencies.CZK, 1.6, 1.5);
+        }
 
-
+        public void Exchange(Account account, Currencies from, Currencies to, double amount)
+        {
+            account[from] -= amount;
+            amount *= GetRates().Where(z => z.Currency == from).FirstOrDefault().BuyRate;
+            amount /= GetRates().Where(z => z.Currency == to).FirstOrDefault().SellRate;
+            account[to] += amount;
         }
     }
 }
